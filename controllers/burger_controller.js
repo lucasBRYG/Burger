08ddc81d732f712d81859bde8dcd6f323d5fbdb4
lucasBRYG <adio.lucas@gmail.com>
2020-11-20 01:00:49 -0500

    adding controllers, models, views and public/assets folders to main directory

---
+++ controllers/burger_controller.js
@@ -0,0 +1,17 @@
+const router = express.Router();
+
+const burger = require("./models/burger.js");
+
+router.get("/", function(req, res) {
+
+});
+
+router.post("/api/burgers", function(req, res) {
+
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+
+});
+
+module.exports = router;

3f8a2154b42e4ec37d6d54ddec0dfebb282b20a8
lucasBRYG <adio.lucas@gmail.com>
2020-11-20 18:29:04 -0500

    fleshed out handlebars, front end js, and burger_controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,17 +1,35 @@
+const express = require("express")
 const router = express.Router();
-
-const burger = require("./models/burger.js");
+const burger = require("../models/burger");
 
 router.get("/", function(req, res) {
-
+    burger.all(function(data) {
+        let hdlbrsOb = {
+            burgers: data
+        };
+        res.render("index", hdlbrsOb);
+    });
 });
 
 router.post("/api/burgers", function(req, res) {
-
+    burger.create(["burger_name", devoured], [req.body.burger_name, req.body.devoured], function(result) {
+        res.json({id: result.insertId})
+    });
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
 
+    burger.update(
+        {
+            devoured: req.body.devoured
+        }, condition, function(result) {
+            if(result.changedRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
 });
 
 module.exports = router;

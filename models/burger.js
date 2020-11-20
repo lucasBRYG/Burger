e4fb1ebf5b2f84f7a9522612cc15ea2885939837
lucasBRYG <adio.lucas@gmail.com>
2020-11-20 14:26:56 -0500

    adding orm object and burger model

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,26 @@
 const orm = require("./config/orm.js");
 
 let burger = {
-
-}
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+    create: function(columns, values, cb) {
+        orm.create("burgers", columns, values, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColumnValues, condition, cb) {
+        orm.update("burgers", objColumnValues, condition, function(res) {
+            cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+            cb(res)
+        });
+    }
+};
 
 module.exports = burger;
